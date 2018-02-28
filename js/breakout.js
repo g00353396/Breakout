03969e914ac35c1d5fccb34a4b39995a673c8301
g00353396 <36000701+g00353396@users.noreply.github.com>
2018-02-28 14:59:59 +0000

    Define a paddle to hit the ball

--- js/breakout.js
+++ js/breakout.js
@@ -10,6 +10,27 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
+//ball variables 
+var ballRadius = 10;
+var ballColour = "0095DD";
+
+//Paddle Variables 
+var paddleHeight = 10 ;
+var paddleWidth = 85;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var paddleColour = 'rgb(' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ')';
+
+//Draw the paddle 
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+    ctx.fillStyle = paddleColour;
+    ctx.fill();
+    ctx.closePath();
+}
+
 //Draw the ball 
 function drawball(){
     ctx.beginPath();
@@ -22,18 +43,49 @@ function draw() {
    
    ctx.clearRect(0,0, canvas.width,canvas.height);
     drawball();
+    drawPaddle();
     x +=dx;
     y +=dy;
     
     if(y+dy > canvas.height-ballRadius||y+dy<ballRadius){
     dy=-dy
-    ballColour = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    ballColour = 'rgb(' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ')';
+    paddleColour = 'rgb(' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ')';
     }
     
     if(x+dx > canvas.width-ballRadius||x+dx<ballRadius){
         dx=-dx
-        ballColour = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        ballColour = 'rgb(' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ')';
+        paddleColour = 'rgb(' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ',' + (Math.floor(Math.random() * 256-30)) + ')';
         }
+    if(rightPressed && paddleX < canvas.width-paddleWidth){
+        paddleX += 8;
+    }
+    else if(leftPressed && paddleX > 0){
+        paddleX -= 8;
+    }
    
+}
+//Listeners 
+document.addEventListener("keydown", keyDownHandler,false);
+document.addEventListener("keyup",keyUpHandler,false);
+
+//key down function
+function keyDownHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37){
+        leftPressed = true;
+    }
+}
+// key up function 
+function keyUpHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37){
+        leftPressed = false;
+    }
 }
 setInterval(draw,10);
